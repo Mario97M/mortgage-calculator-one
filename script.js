107c0c0c4c5dc23d2b21306c6356e9fb6e0fee82
Mario Meza <mariomeza093@gmail.com>
2023-08-07 18:51:41 -0700

    Added the javascript code

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const sliders = document.querySelectorAll(".slider");
+const purchasePriceDisplay = document.getElementById("purchasePriceDisplay");
+const purchasePriceRange = document.getElementById("purchasePriceRange");
+const equityDisplay = document.getElementById("equityDisplay");
+const equityDisplayRange = document.getElementById("equityDisplayRange");
+const repaymentTimeDisplay = document.getElementById("repaymentTimeDisplay");
+const repaymentTimeRange = document.getElementById("repaymentTimeRange");
+const loanAmount = document.getElementById("loanAmount");
+const estPerMonth = document.getElementById("estPerMonth");
+const submitButton = document.getElementById("submitButton");
 
 function slideColorBackground(sliderElement) {
   sliderElement.addEventListener("input", () => {
@@ -11,3 +20,44 @@ function slideColorBackground(sliderElement) {
 }
 
 sliders.forEach(sliderElement => slideColorBackground(sliderElement));
+
+function mortgageCalculator() {
+  const purchasePriceRangeValue = parseFloat(purchasePriceRange.value);
+  const equityDisplayRangeValue = parseFloat(equityDisplayRange.value);
+  const repaymentTimeRangeValue = parseFloat(repaymentTimeRange.value);
+  const errorMessage = document.getElementById("error");
+
+  const loanAmountForumula = purchasePriceRangeValue - equityDisplayRangeValue;
+  loanAmount.textContent = `$${loanAmountForumula.toFixed(2)}`;
+
+  const monthlyInterestRateFormula = 0.05 / 12;
+  const numberOfPaymentsFormula = repaymentTimeRangeValue * 12;
+  const monthlyPaymentFormula =
+    (loanAmountForumula *
+      (monthlyInterestRateFormula *
+        Math.pow(1 + monthlyInterestRateFormula, numberOfPaymentsFormula))) /
+    (Math.pow(1 + monthlyInterestRateFormula, numberOfPaymentsFormula) - 1);
+
+  estPerMonth.textContent = `$${monthlyPaymentFormula.toFixed(2)}`;
+
+  if (purchasePriceRangeValue < equityDisplayRangeValue) {
+    errorMessage.textContent =
+      "Purchase price cannot be less than the down payment";
+  } else {
+    errorMessage.textContent = "";
+  }
+}
+
+purchasePriceRange.addEventListener("input", function () {
+  purchasePriceDisplay.textContent = `$${this.value}`;
+});
+
+equityDisplayRange.addEventListener("input", function () {
+  equityDisplay.textContent = `$${this.value}`;
+});
+
+repaymentTimeRange.addEventListener("input", function () {
+  repaymentTimeDisplay.textContent = `$${this.value}`;
+});
+
+submitButton.addEventListener("click", mortgageCalculator);
